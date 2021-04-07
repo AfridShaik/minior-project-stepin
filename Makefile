2b77f8424eb9c712dcc56ba73041f2a5016c8308
AfridShaik <afridtech1@gmail.com>
2021-04-07 17:31:21 +0530

    Make File Not Working

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-test.exe: main.c funtion.c 
+test.exe : main.c funtion.c 
     gcc main.c funtion.c -o test.exe
+
+run : test.exe
+    test.exe
--- funtion.c
+++ funtion.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
+typedef struct varaible{
+unsigned int mid1,mid2,mid3;
+} varaible;
 
 int mid_marks_calculate(){
-    unsigned int mid1,mid2,mid3;
+    varaible v;
     int sum,mid_internal_marks;
     printf("Enther the mid marks by giving space between them\n");
-    scanf("%u %u %u",&mid1,&mid2,&mid3);
-    sum = (mid1*0.4)+(mid2*0.4)+(mid3*0.4);
+    scanf("%u %u %u",&v.mid1,&v.mid2,&v.mid3);
+    sum = (v.mid1*0.4)+(v.mid2*0.4)+(v.mid3*0.4);
     mid_internal_marks = sum * 0.6;
     printf("The Mid Internal Marks are %d",mid_internal_marks);
 }
