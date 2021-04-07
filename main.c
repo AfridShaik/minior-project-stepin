e47117b6d9158040b9e0d7fe38cb9ef50c11380e
AfridShaik <afridtech1@gmail.com>
2021-04-07 17:20:51 +0530

    updated Makefile

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include<C:\Users\afrid\OneDrive\Documents\minior project\funtion.h>
 int main(){
     int n;
+    printf("Select An option\n1 - Mid Marks\n2 - Internal Marks\n3 - Semister Examinations\n");
     scanf("%d",&n);
     switch(n) {
         case 1:
